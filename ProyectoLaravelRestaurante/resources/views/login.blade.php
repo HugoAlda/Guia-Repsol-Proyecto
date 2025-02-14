122e9a13c4b4c6e6dc6c60d571cc514f2085d4a6
HugoAlda <hugoa1204@gmail.com>
2025-02-14 18:13:34 +0100

    css login y registro

--- ProyectoLaravelRestaurante/resources/views/login.blade.php
+++ ProyectoLaravelRestaurante/resources/views/login.blade.php
@@ -6,44 +6,22 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
     <title>Login</title>
-    <style>
-        /* Estilos adicionales para los botones */
-        .boton-cambio {
-            background-color: #007bff;
-            color: white;
-            border: none;
-            padding: 10px 20px;
-            margin: 5px;
-            cursor: pointer;
-            border-radius: 5px;
-            transition: background-color 0.3s ease;
-        }
-
-        .boton-cambio:hover {
-            background-color: #0056b3;
-        }
-
-        .enlace {
-            color: #007bff;
-            text-decoration: none;
-            margin-top: 10px;
-            display: block;
-        }
-
-        .enlace:hover {
-            text-decoration: underline;
-        }
-    </style>
 </head>
 <body>
     <div class="fondo-difuminado">
         <div class="form">
             <div class="contenedor-izquierda">
-                <h1 class="titulo">Login Guia Repsol</h1>
+                <br>
+                <h1 class="titulo">¡Bienvenido a la Guia Repsol!</h1>
+                <p>En este video explicamos básicamente que es lo que fometamos y que ventajas puedes obtener.</p>
+                <br>
+                <div class="contenedor-video">
+                    <a href="https://youtu.be/cR395YajkeM"><img src="{{asset('img/video.png')}}"></a>
+                </div>
             </div>
             <div class="contenedor-derecha">
                 <div class="cambio">
-                    <button class="boton-cambio" onclick="mostrarFormulario('login')">Iniciar sesión</button>
+                    <button class="boton-cambio active" onclick="mostrarFormulario('login')">Iniciar sesión</button>
                     <button class="boton-cambio" onclick="mostrarFormulario('registro')">Registrarse</button>
                 </div>
                 <div class="login-register">
