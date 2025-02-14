a6e037d8f8c48aae8c1490c46bac446b6a86e192
alejandr2004 <145560106+alejandr2004@users.noreply.github.com>
2025-02-14 15:04:17 +0100

    Add files via upload

---
+++ ProyectoLaravelRestaurante/database/migrations/0001_01_01_000005_create_restaurantes_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('restaurantes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_restaurantes');
+            $table->string('nombre_restaurante', 100);
+            $table->string('ubicacion_restaurante', 191);
+            $table->text('descripcion_restaurante')->nullable();
+            $table->string('horario_restaurante', 50)->nullable();
+            $table->decimal('precio_restaurante', 10, 2)->nullable();
+            $table->decimal('valoracion_media', 3, 2)->default(0);
+            $table->string('nombre_gerente', 50)->nullable();
+            $table->string('email_gerente', 100)->unique()->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('restaurantes');
+    }
+};
