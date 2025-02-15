adc6fffd1a7792f9f3bc9a2d79e0b5f3c42c9dca
HugoAlda <hugoa1204@gmail.com>
2025-02-15 23:18:33 +0100

    Añadidos seeders restaurantes

--- ProyectoLaravelRestaurante/database/migrations/0001_01_01_000005_create_restaurantes_table.php
+++ ProyectoLaravelRestaurante/database/migrations/0001_01_01_000005_create_restaurantes_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->decimal('valoracion_media', 3, 2)->default(0);
             $table->string('img_restaurante')->nullable();
             $table->string('nombre_gerente', 50)->nullable();
-            $table->string('email_gerente', 100)->unique()->nullable();
+            $table->string('email_gerente', 100)->nullable();
             $table->timestamps();
         });
     }
