039a5cac60d0ef364d70cdc9ad7a18cc32ec09b2
Marc Martinez <mxrcxs.mxrtxnez@gmail.com>
2025-02-16 20:43:58 +0100

    Imagen corregida + blade Guia Repsol y Restaurante Controller

--- ProyectoLaravelRestaurante/resources/views/guia-repsol.blade.php
+++ ProyectoLaravelRestaurante/resources/views/guia-repsol.blade.php
@@ -13,8 +13,10 @@
             <a class="btn-access" href="#">Acceso</a>
         </div>
     </nav>
+    
     <div class="container">
         <h2 class="titulo">Descubre los galardonados con Estrellas Guía Repsol de 2024</h2>
+        
         <div class="filters">
             <button class="filter-btn">5 Estrellas</button>
             <button class="filter-btn">4 Estrellas</button>
@@ -22,17 +24,17 @@
             <button class="filter-btn">2 Estrellas</button>
             <button class="filter-btn">1 Estrella</button>
         </div>
-        
-        <!-- Agrupar restaurantes por rangos de valoración -->
+
+        <!-- Mostrar restaurantes ordenados de mayor a menor estrellas -->
         @foreach ($restaurantesByRating as $range => $restaurantes)
             @if ($restaurantes->isNotEmpty())
                 <div class="restaurant-category">
                     <div class="category-info">
-                        <h3>{{ str_replace(',', '-', $range) }} Estrellas</h3>
+                        <h3>⭐ {{ $range }} Estrellas</h3>
                         <h5 class="comunidad">Andalucía</h5> <!-- Puedes hacer esto dinámico también -->
                     </div>
                     <div class="restaurant-cards-container">
-                        @foreach ($restaurantes as $restaurante)
+                        @foreach ($restaurantes->sortByDesc('valoracion_media') as $restaurante)
                             <div class="restaurant-card">
                                 <img src="{{ asset('img/logos_restaurantes/' . $restaurante->img_restaurante) }}" alt="{{ $restaurante->nombre_restaurante }}">
                                 <div class="info">
