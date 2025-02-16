e67cdccb00fe21ef4f6608eff36381de826b378c
ErikPenas02 <epenyasandrea@gmail.com>
2025-02-16 18:15:33 +0100

    Merge hecho ya

--- ProyectoLaravelRestaurante/resources/views/guia-repsol.blade.php
+++ ProyectoLaravelRestaurante/resources/views/guia-repsol.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Guía Repsol 2024</title>
-    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <link rel="stylesheet" href="{{ phpasset('css/styles.css') }}">
 </head>
 <body>
     <nav class="navbar">
